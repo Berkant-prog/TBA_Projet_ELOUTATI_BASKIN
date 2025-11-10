f3f569a204bd75026616ca4642eb17716763a4df
berkant-baskin <berkant.baskin@edu.esiee.fr>
2025-11-10 17:09:26 +0100

    Create unit tests for game mechanics
    Added unit tests for various game functionalities.

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
+# test.py
+# Tests automatiques du jeu.
+
+class Test:
+    """Classe regroupant les tests unitaires."""
+
+    def test_combat(self):
+        """Vérifie le bon déroulement d’un combat."""
+        pass
+
+    def test_items(self):
+        """Vérifie l’application correcte des effets d’objet."""
+        pass
+
+    def test_navigation(self):
+        """Vérifie que les déplacements entre planètes fonctionnent."""
+        pass
+
+    def test_inventory(self):
+        """Vérifie l’ajout et la suppression d’objets."""
+        pass
+
+    def test_endings(self):
+        """Simule les fins du jeu selon les variables."""
+        pass
 
