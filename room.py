a281426bbda2f86fc7d511f0fdd93ccf0ea0f1bc
berkant-baskin <berkant.baskin@edu.esiee.fr>
2025-11-10 17:08:18 +0100

    Implement Room class with basic methods
    Defines the Room class with methods for room management.

--- room.py
+++ room.py
@@ -1,1 +1,34 @@
+# room.py
+# Représente les planètes / lieux du jeu.
+
+class Room:
+    """Classe représentant un lieu ou une planète."""
+
+    def __init__(self, name, description, connected_rooms=None, items=None, pnj=None):
+        """Initialise la salle avec ses éléments."""
+        pass
+
+    def describe(self):
+        """Affiche la description du lieu."""
+        pass
+
+    def get_connections(self):
+        """Retourne les directions possibles."""
+        pass
+
+    def add_item(self, item):
+        """Ajoute un objet dans la salle."""
+        pass
+
+    def remove_item(self, item):
+        """Retire un objet de la salle."""
+        pass
+
+    def has_enemy(self):
+        """Vérifie s’il y a un ennemi dans la salle."""
+        pass
+
+    def get_enemy(self):
+        """Retourne l’ennemi présent s’il existe."""
+        pass
 
